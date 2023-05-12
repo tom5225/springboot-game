e9f23fc6da1975046f57f2860a680241c1eae54b
Tom~ <gonsakon@gmail.com>
2023-05-12 10:00:51 +0800

    初始化:springboot

---
+++ springboot-game/src/main/java/com/example/springbootgame/SpringbootGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGameApplication.class, args);
+    }
+
+}
---
+++ springboot-game/src/test/java/com/example/springbootgame/SpringbootGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
